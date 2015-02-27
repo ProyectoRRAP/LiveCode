94154c4563c9ab4c866345001cba87e46a0c4df2
roamarox <amaro.rodrigo1@gmail.com>
2015-02-27 20:47:57 -0200

    Se agrego modulo para construir nodos con interfaces, se agrego el modulo datatypes creado por emi

---
+++ utils/datatypes.py
@@ -0,0 +1,155 @@
+'''
+Created on Feb 27, 2015
+
+@author: efviodo
+'''
+
+
+
+##########################################################################
+########## DTNODE
+##########################################################################
+class DTNode(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, router_id, datapath_id, interfaces, status=None, net_type=None, 
+                 top_type=None, description=None):
+        '''
+        Constructor
+        '''
+        
+        self.router_id = router_id
+        self.datapath_id = datapath_id
+        self.status = status
+        self.net_type = net_type
+        self.top_type = top_type
+        self.description = description
+        self.interfaces = interfaces
+        
+##########################################################################
+########## DTINTERFACE
+##########################################################################
+class DTInterface(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, ip_address, link=None, mac_address=None, ovs_port=None):
+        '''
+        Constructor
+        '''
+        
+        self.ip_address = ip_address
+        self.mac_address = mac_address
+        self.ovs_port = ovs_port
+        self.link = link
+        
+##########################################################################
+########## DTLINK
+##########################################################################   
+class DTLink(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, from_node, to_node, status):
+        '''
+        Constructor
+        '''
+        
+        self.from_node = from_node
+        self.to_node = to_node
+        status = status
+
+##########################################################################
+########## DTLSNODE
+##########################################################################          
+class DTLSNode(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, router_id, status=1, interfaces=None):
+        '''
+        Constructor
+        '''
+        
+        
+        self.router_id = router_id
+        self.status = status
+        
+        if interfaces is None:
+            self.interfaces = {}
+        else:
+            self.interfaces = interfaces     
+            
+##########################################################################
+########## DTLSINTERFACE
+##########################################################################      
+class DTLSInterface(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, node, ip_address, status=1, links=None):
+        '''
+        Constructor
+        '''
+        
+        self.node = node
+        self.ip_address = ip_address
+        self.status = status
+        if links is None:
+            self.links = {}
+        else:
+            self.links = links
+        
+    def __eq__(self, other):
+        if isinstance(other, DTLSInterface):
+            return self.ip_address == other.ip_address 
+        else:
+            return NotImplemented
+        
+    def __ne__(self, other):
+        result = self.__eq__(other)
+        if result is NotImplemented:
+            return result
+        return not result
+       
+##########################################################################
+########## DTLSINK
+##########################################################################   
+class DTLSLink(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, from_node_int, to_node_int, status=1):
+        '''
+        Constructor
+        '''
+        
+        self.from_node_int = from_node_int
+        self.to_node_int = to_node_int
+        status = status
+        
+    def __eq__(self, other):
+        if isinstance(other, DTLSLink):
+            return self.from_node == other.from_node and self.to_node == other.to_node
+        else:
+            return NotImplemented
+        
+    def __ne__(self, other):
+        result = self.__eq__(other)
+        if result is NotImplemented:
+            return result
+        return not result 
+
